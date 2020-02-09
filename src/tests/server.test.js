d02dde526929a9b2312c244d2a765c1846462a2e
Noble Obioma <nobioma1@gmail.com>
2020-02-09 23:40:23 +0100

    Test calculatePrice resolver for SELL type

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -10,6 +10,21 @@ const GET_INFO = gql`
   }
 `;
 
+const CALCULATE_PRICE = gql`
+  query calculatePrice(
+    $type: TypeFields!
+    $margin: Float!
+    $exchangeRate: Float!
+  ) {
+    calculatePrice(type: $type, margin: $margin, exchangeRate: $exchangeRate) {
+      value
+      currency
+    }
+  }
+`;
+
+jest.mock('axios');
+
 describe('Server', () => {
   test('[query info] Get API information', async () => {
     const { server } = constructTestServer();
@@ -20,4 +35,40 @@ describe('Server', () => {
     expect(res.data).toEqual({ info: 'Welcome to BuyCoinsGraphQl Challenge' });
   });
 
+  test('[query calculatePrice] Calculate "SELL"', async () => {
+    const { server } = constructTestServer();
+    const { query } = createTestClient(server);
+
+    const rate = 20093.92;
+    const exchangeRate = 358;
+    const margin = 0.2;
+    const computedMargin = margin * rate;
+
+    const mockRes = {
+      data: {
+        bpi: {
+          USD: {
+            code: 'USD',
+            rate: `${rate}`,
+            description: 'United States Dollar',
+            rate_float: rate,
+          },
+        },
+      },
+    };
+
+    axios.get.mockResolvedValue(mockRes);
+
+    const res = await query({
+      query: CALCULATE_PRICE,
+      variables: { type: 'sell', margin, exchangeRate },
+    });
+
+    expect(res.data).toEqual({
+      calculatePrice: {
+        value: (rate - computedMargin) * exchangeRate,
+        currency: 'NGN',
+      },
+    });
+  });
 });
